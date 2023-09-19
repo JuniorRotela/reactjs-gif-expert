9e77dacc4a891fede671bac480d6e8f50a7f556d
Junior Dario Rotela Fretes <juniorrotela@MacBook-Pro-de-Junior.local>
2023-09-19 18:27:32 -0400

    update

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,8 +1,8 @@
 
 
 export const GifItem = ({title, url, id}) => {
-    console.log({title, url,id});
-  return (
+    // console.log({title, url,id});
+      return (
     
     <div className="card">
         <img src={url} alt={title}/>
